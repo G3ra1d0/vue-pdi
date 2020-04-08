5692774a1ab51a7368c361a51e0978763af7a6ab
G3ra1d0 <geraldo-vitor@hotmail.com>
2020-04-08 10:00:56 -0400

    Criação do Componente UploadArquivos

---
+++ src/components/UploadArquivo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <q-btn flat dense round icon="add" @click="onUpload" />
+    <input
+      ref="file"
+      class="hidden"
+      type="file"
+      multiple="multiple"
+      accept="image/*"
+      @change="onFileChanged"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UploadArquivo",
+  data() {
+    return {};
+  },
+  methods: {
+    onFileChanged(event) {
+      let file = event.target.files[0];
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        this.$store.dispatch("file/setBase64", reader.result);
+      };
+      this.$store.dispatch("file/setFile", file);
+    },
+    onUpload() {
+      this.$refs.file.value = null;
+      this.$refs.file.click();
+    }
+  }
+};
+</script>
