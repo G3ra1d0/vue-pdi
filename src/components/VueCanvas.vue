dbaa39c76da5faf1ccab68aef6188ebc6af86361
G3ra1d0 <geraldo-vitor@hotmail.com>
2020-04-08 10:25:26 -0400

    Passa Mouse por cima pega cor do Pixel e a posição

--- src/components/VueCanvas.vue
+++ src/components/VueCanvas.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
     <!-- <canvas @mousemove="hover" :height="file.height" id="IdCanvas"></canvas> -->
-    <canvas :width="largura" :height="altura" id="IdCanvas"></canvas>
-    {{ largura }}
-    {{ altura }}
+    <canvas
+      @mousemove="hover"
+      @mouseout="mouseout"
+      :width="largura"
+      :height="altura"
+      id="IdCanvas"
+    ></canvas>
+    <p>Tamanhdo da Imagem l x h: {{ largura }} X {{ altura }}</p>
+    <p>Posicao do Mouse Y: {{ pixelY }} Posicao do Mouse X: {{ pixelX }}</p>
+    <p>Cor do Pixel (X, Y): {{ pixelCor }}</p>
   </div>
 </template>
 
@@ -11,7 +18,11 @@
 export default {
   name: "VueCanvas",
   data() {
-    return {};
+    return {
+      pixelX: "",
+      pixelY: "",
+      pixelCor: ""
+    };
   },
   watch: {
     base64() {
@@ -27,7 +38,6 @@ export default {
         setTimeout(() => {
           this.context.drawImage(img, 0, 0, this.largura, this.altura);
         }, 100);
-        
       };
     }
   },
@@ -49,6 +59,20 @@ export default {
     }
   },
   methods: {
+    mouseout(e) {
+      this.pixelX = "";
+      this.pixelY = "";
+      this.pixelCor = `RGB()`;
+    },
+
+    hover(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      let imageData = this.context.getImageData(e.layerX, e.layerY, 1, 1);
+      this.pixelX = e.layerX;
+      this.pixelY = e.layerY;
+      this.pixelCor = `RGB(${imageData.data[0]},${imageData.data[1]},${imageData.data[2]})`;
+    },
     limpar() {
       this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
