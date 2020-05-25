57cf801a0f379a20bb4bc418af2dd8ace2d91b4a
G3ra1d0 <geraldo-vitor@hotmail.com>
2020-05-25 20:50:43 -0300

    Filtro Mediana

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -75,6 +75,12 @@
               <q-item-label caption>Aplica Filtro Raiz na imagem</q-item-label>
             </q-item-section>
           </q-item>
+          <q-item clickable v-ripple @click="filtroMediana">
+            <q-item-section>
+              <q-item-label>Mediana</q-item-label>
+              <q-item-label caption>Filtro Mediana</q-item-label>
+            </q-item-section>
+          </q-item>
 
          <q-expansion-item
         expand-separator
@@ -435,6 +441,61 @@ export default {
     }
   },
   methods: {
+    async filtroMediana(){
+      await this.resertImagem().then(result => {
+        let ImageData = this.getImageData();
+        let pixel,
+          posicao = 0,
+          red,
+          green,
+          blue,
+          temp;
+        for (let y = 0; y < this.altura; y++) {
+          for (let x = 0; x < this.largura; x++) {
+            if (
+              x == 0 ||
+              y == 0 ||
+              y == this.altura - 1 ||
+              x == this.largura - 1
+            ) {
+              pixel = this.context.getImageData(
+                this.largura - x,
+                this.altura - y,
+                1,
+                1
+              ).data;
+              ImageData.data[posicao] = pixel[0];
+              ImageData.data[posicao + 1] = pixel[1];
+              ImageData.data[posicao + 2] = pixel[2];
+              ImageData.data[posicao + 3] = pixel[3];
+            } else {
+              pixel = this.context.getImageData(x - 1, y - 1, 3, 3).data;
+
+              red = [];
+              green = [];
+              blue = [];
+
+              for (temp = 0; temp < 36; temp += 4) {
+                red.push( pixel[temp]);
+                green.push( pixel[temp + 1]);
+                blue.push( pixel[temp + 2]);
+              }
+
+               red =  red.sort()             
+               green =  green.sort()
+               blue =  blue.sort()
+
+              ImageData.data[posicao] = red[4];
+              ImageData.data[posicao + 1] = red[4];
+              ImageData.data[posicao + 2] = red[4];
+            }
+
+            posicao += 4;
+          }
+        }
+        this.putImageData(ImageData);
+      });
+    },
     compensao(){
       this.resertImagem().then(result => {
         let ImageData = this.getImageData();
